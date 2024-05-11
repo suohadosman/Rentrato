1f35dee879bfc923f286417326566e872bc289a8
suohadosman <95607060+suohadosman@users.noreply.github.com>
2024-05-11 16:07:36 +0400

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,32 @@
+/******** Swiper View ********/
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    /***  Initialize Swiper **/
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  } else {
+    /***  Initialize Swiper **/
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+}
+
+var x = window.matchMedia("(max-width: 720px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+
